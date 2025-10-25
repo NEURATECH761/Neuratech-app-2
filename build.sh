961aac7f542f28582d9776a8e8161cf2f015e33c
Kake Ousman <noahk8540@gmail.com>
2025-10-25 13:26:38 +0100

    fix: Correction du script de build Vercel

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Étape 1: Télécharger Flutter
+# Étape 1: Télécharger la version stable de Flutter
 git clone https://github.com/flutter/flutter.git --branch stable
 
-# Étape 2: Ajouter Flutter au PATH (pour que les commandes soient plus courtes )
+# Étape 2: Ajouter Flutter au "chemin" pour que les commandes fonctionnent
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Étape 3: Configurer le projet pour le web
+# Étape 3: CRÉER LA CONFIGURATION WEB (L'étape que j'avais oubliée )
 flutter create . --platforms web
 
-# Étape 4: Nettoyer et construire le projet
+# Étape 4: Nettoyer le projet
 flutter clean
+
+# Étape 5: Construire le projet pour le web avec les clés Supabase
 flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
